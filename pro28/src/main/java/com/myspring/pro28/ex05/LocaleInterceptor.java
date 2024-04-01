5ca81bf08fd1ae667f8b83a0d123be2e1d721f2e
KorCarrot <rlawjdrms30@gmail.com>
2024-04-01 23:39:47 +0900

    2024/04/01 11:39 P.M. 집에서 git clone 이후 pro28, 29, 30 추가.

--- pro28/src/main/java/com/myspring/pro28/ex05/LocaleInterceptor.java
+++ pro28/src/main/java/com/myspring/pro28/ex05/LocaleInterceptor.java
@@ -1,39 +1,34 @@
 package com.myspring.pro28.ex05;
 
+import java.util.Locale;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+public class LocaleInterceptor extends  HandlerInterceptorAdapter{
+	   @Override
+	   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
+	      HttpSession session=request.getSession();
+	      String locale=request.getParameter("locale");
+	      if(locale==null)
+	         locale="ko";
+	      session.setAttribute("org.springframework.web.servlet.i18n.SessionLocaleResolver.LOCALE",new Locale(locale));
+	      return true;
+	   }
 
-public class LocaleInterceptor extends HandlerInterceptorAdapter{
+	   @Override
+	   public void postHandle(HttpServletRequest request, HttpServletResponse response,
+	                           Object handler, ModelAndView modelAndView) throws Exception {
+	   }
 
-	private static final Logger logger = LoggerFactory.getLogger(LocaleInterceptor.class);
-	
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-			throws Exception {
-		logger.info("LocaleInterceptor");
-		//locale 속성을 request에서 가져와서 세션에다가 저장
-		
-		return super.preHandle(request, response, handler);
+	   @Override
+	   public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+	                                    Object handler, Exception ex)    throws  Exception {
+	   }
 	}
-	
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
-	
-		
-	}
-	
-	@Override
-	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
-			throws Exception {
-	
-		
-	}
-	
-}
